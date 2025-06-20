0366e377e440879be7d86154ec6b9018dc4f3e9f
AutoBiz <autobizglobal@gmail.com>
2025-06-20 05:27:54 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++
@@ -1,5 +0,0 @@
-
-// This file is intentionally left blank.
-// It was part of the Clerk authentication setup which has been removed.
-// Keeping the file structure temporarily to avoid 404s if old links are hit,
-// but it no longer defines a page.
